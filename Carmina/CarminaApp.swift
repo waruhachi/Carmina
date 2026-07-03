0524abe2c4e08538ec86279a9eecff17f2a5802f
waru <156133757+waruhachi@users.noreply.github.com>
2026-07-03 16:50:06 -0500

    Initial Commit

---
+++ Carmina/CarminaApp.swift
@@ -0,0 +1,32 @@
+//
+//  CarminaApp.swift
+//  Carmina
+//
+//  Created by waru on 7/3/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CarminaApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Carmina/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Carmina
+//
+//  Created by waru on 7/3/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
